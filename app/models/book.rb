3feb2ac2d6519fde6e17e2f433152af65d17804e
Monica Seco <seco.monica@gmail.com>
2015-03-03 12:29:51 +0100

    Made first sale

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
 	extend FriendlyId
-	friendly_id :name, use: :slugged	
+	friendly_id :name, use: :slugged
+
 	belongs_to :user
 	has_many :sales
 	has_attached_file :image
@@ -18,5 +19,5 @@ class Book < ActiveRecord::Base
 	validates :resource, attachment_presence: true
 
 	validates_numericality_of :price,
-	greater_than :49, message: "must be at least 50 cents"
+	greater_than: 49, message: "must be at least 50 cents"
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,4 @@
-Rails.configuration.srtipe = {
+Rails.configuration.stripe = {
 	publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
 	secret_key: ENV['STRIPE_SECRET_KEY']
 }
