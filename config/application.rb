4acd55195d17904b165a6452dc6eace522333743
Pedro Fernandes da Silva <paca.fs@gmail.com>
2015-04-10 16:43:43 +0100

    Bucket

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module Estuk
 
     config.paperclip_defaults = {
         storage: :s3,
-        s3_host_name: "s3-us-west-2.amazonaws.com",
         s3_credentials: {
             bucket: ENV['AWS_BUCKET'],
             access_key_id: ENV['AWS_ACESS_KEY_ID'],
@@ -35,3 +34,6 @@ module Estuk
 
   end
 end
+
+
+
